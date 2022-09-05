38900607434249eb907cf8acbf3720cd55c8ebd1
Dan Brough <dan@danbrough.org>
2022-09-06 11:14:18 +1200

    update

--- publish.sh
+++ publish.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd $(dirname "$0")
 
-export PUBLISHING_URL="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+export PUBLISHING_URL="https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
 export PUBLISHING_USER="danbrough"
 
 if [ -z "$PUBLISHING_PASSWORD" ]; then
     echo PUBLISHING_PASSWORD not set
     exit 1
 fi
-./gradlew tasks
+
+if [ "$(uname)" = "Darwin" ]; then
+  ./gradlew `cat mac_targets.txt`
+  exit 0
+fi
+
+./gradlew publishAllPublicationsToMavenRepository
 
